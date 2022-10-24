08c6c3c9ee495f2282d245b0d30ff03dec245491
YurahSafedien <91724886+YurahSafedien@users.noreply.github.com>
2022-10-24 20:02:22 +0200

    Added drag and drop
    Can drag and drop, bit of an error with changing label names tho.

--- MatchGame.Designer.cs
+++ MatchGame.Designer.cs
@@ -30,22 +30,53 @@ namespace DeweyDecimalStacking
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MatchGame));
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flp_Unsorted = new System.Windows.Forms.FlowLayoutPanel();
             this.btn_MatchHelp = new System.Windows.Forms.Button();
             this.btn_MatchHome = new System.Windows.Forms.Button();
             this.btn_MatchStart = new System.Windows.Forms.Button();
             this.btn_MatchHint = new System.Windows.Forms.Button();
             this.btn_MatchCheck = new System.Windows.Forms.Button();
             this.flp_Boxes = new System.Windows.Forms.FlowLayoutPanel();
+            this.flp_Sorted1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.flp_Sorted10 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flp_Sorted9 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flp_Sorted8 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flp_Sorted7 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flp_Sorted6 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flp_Sorted5 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flp_Sorted2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flp_Sorted3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flp_Sorted4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flp_Unsorted.SuspendLayout();
             this.SuspendLayout();
             // 
-            // flowLayoutPanel1
+            // flp_Unsorted
             // 
-            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 475);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1016, 124);
-            this.flowLayoutPanel1.TabIndex = 0;
+            this.flp_Unsorted.BackColor = System.Drawing.Color.Transparent;
+            this.flp_Unsorted.Controls.Add(this.label1);
+            this.flp_Unsorted.Controls.Add(this.label2);
+            this.flp_Unsorted.Controls.Add(this.label3);
+            this.flp_Unsorted.Controls.Add(this.label4);
+            this.flp_Unsorted.Controls.Add(this.label5);
+            this.flp_Unsorted.Controls.Add(this.label6);
+            this.flp_Unsorted.Controls.Add(this.label7);
+            this.flp_Unsorted.Controls.Add(this.label8);
+            this.flp_Unsorted.Controls.Add(this.label9);
+            this.flp_Unsorted.Controls.Add(this.label10);
+            this.flp_Unsorted.Location = new System.Drawing.Point(12, 477);
+            this.flp_Unsorted.Name = "flp_Unsorted";
+            this.flp_Unsorted.Size = new System.Drawing.Size(1016, 122);
+            this.flp_Unsorted.TabIndex = 0;
             // 
             // btn_MatchHelp
             // 
@@ -107,6 +138,156 @@ namespace DeweyDecimalStacking
             this.flp_Boxes.Size = new System.Drawing.Size(1008, 40);
             this.flp_Boxes.TabIndex = 14;
             // 
+            // flp_Sorted1
+            // 
+            this.flp_Sorted1.Location = new System.Drawing.Point(12, 281);
+            this.flp_Sorted1.Name = "flp_Sorted1";
+            this.flp_Sorted1.Size = new System.Drawing.Size(80, 95);
+            this.flp_Sorted1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 95);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(89, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 95);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(175, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 95);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(261, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 95);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(347, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 95);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(433, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 95);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(519, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 95);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(605, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 95);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(691, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(80, 95);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(777, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(80, 95);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "label10";
+            // 
+            // flp_Sorted10
+            // 
+            this.flp_Sorted10.Location = new System.Drawing.Point(900, 281);
+            this.flp_Sorted10.Name = "flp_Sorted10";
+            this.flp_Sorted10.Size = new System.Drawing.Size(80, 95);
+            this.flp_Sorted10.TabIndex = 16;
+            // 
+            // flp_Sorted9
+            // 
+            this.flp_Sorted9.Location = new System.Drawing.Point(803, 281);
+            this.flp_Sorted9.Name = "flp_Sorted9";
+            this.flp_Sorted9.Size = new System.Drawing.Size(80, 95);
+            this.flp_Sorted9.TabIndex = 16;
+            // 
+            // flp_Sorted8
+            // 
+            this.flp_Sorted8.Location = new System.Drawing.Point(706, 281);
+            this.flp_Sorted8.Name = "flp_Sorted8";
+            this.flp_Sorted8.Size = new System.Drawing.Size(80, 95);
+            this.flp_Sorted8.TabIndex = 16;
+            // 
+            // flp_Sorted7
+            // 
+            this.flp_Sorted7.Location = new System.Drawing.Point(607, 281);
+            this.flp_Sorted7.Name = "flp_Sorted7";
+            this.flp_Sorted7.Size = new System.Drawing.Size(80, 95);
+            this.flp_Sorted7.TabIndex = 16;
+            // 
+            // flp_Sorted6
+            // 
+            this.flp_Sorted6.Location = new System.Drawing.Point(507, 281);
+            this.flp_Sorted6.Name = "flp_Sorted6";
+            this.flp_Sorted6.Size = new System.Drawing.Size(80, 95);
+            this.flp_Sorted6.TabIndex = 16;
+            // 
+            // flp_Sorted5
+            // 
+            this.flp_Sorted5.Location = new System.Drawing.Point(406, 281);
+            this.flp_Sorted5.Name = "flp_Sorted5";
+            this.flp_Sorted5.Size = new System.Drawing.Size(80, 95);
+            this.flp_Sorted5.TabIndex = 16;
+            // 
+            // flp_Sorted2
+            // 
+            this.flp_Sorted2.Location = new System.Drawing.Point(110, 281);
+            this.flp_Sorted2.Name = "flp_Sorted2";
+            this.flp_Sorted2.Size = new System.Drawing.Size(80, 95);
+            this.flp_Sorted2.TabIndex = 16;
+            // 
+            // flp_Sorted3
+            // 
+            this.flp_Sorted3.Location = new System.Drawing.Point(205, 281);
+            this.flp_Sorted3.Name = "flp_Sorted3";
+            this.flp_Sorted3.Size = new System.Drawing.Size(80, 95);
+            this.flp_Sorted3.TabIndex = 16;
+            // 
+            // flp_Sorted4
+            // 
+            this.flp_Sorted4.Location = new System.Drawing.Point(304, 281);
+            this.flp_Sorted4.Name = "flp_Sorted4";
+            this.flp_Sorted4.Size = new System.Drawing.Size(80, 95);
+            this.flp_Sorted4.TabIndex = 16;
+            // 
             // MatchGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -114,28 +295,59 @@ namespace DeweyDecimalStacking
             this.AutoSize = true;
             this.BackgroundImage = global::DeweyDecimalStacking.Properties.Resources.Bg_NewMatchBooks;
             this.ClientSize = new System.Drawing.Size(1040, 660);
+            this.Controls.Add(this.flp_Sorted10);
+            this.Controls.Add(this.flp_Sorted9);
+            this.Controls.Add(this.flp_Sorted8);
+            this.Controls.Add(this.flp_Sorted7);
+            this.Controls.Add(this.flp_Sorted6);
+            this.Controls.Add(this.flp_Sorted5);
+            this.Controls.Add(this.flp_Sorted2);
+            this.Controls.Add(this.flp_Sorted3);
+            this.Controls.Add(this.flp_Sorted4);
+            this.Controls.Add(this.flp_Sorted1);
             this.Controls.Add(this.flp_Boxes);
             this.Controls.Add(this.btn_MatchCheck);
             this.Controls.Add(this.btn_MatchHint);
             this.Controls.Add(this.btn_MatchStart);
             this.Controls.Add(this.btn_MatchHome);
             this.Controls.Add(this.btn_MatchHelp);
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.flp_Unsorted);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MatchGame";
             this.Text = "MatchGame";
+            this.flp_Unsorted.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flp_Unsorted;
         private System.Windows.Forms.Button btn_MatchHelp;
         private System.Windows.Forms.Button btn_MatchHome;
         private System.Windows.Forms.Button btn_MatchStart;
         private System.Windows.Forms.Button btn_MatchHint;
         private System.Windows.Forms.Button btn_MatchCheck;
         private System.Windows.Forms.FlowLayoutPanel flp_Boxes;
+        private System.Windows.Forms.FlowLayoutPanel flp_Sorted1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.FlowLayoutPanel flp_Sorted10;
+        private System.Windows.Forms.FlowLayoutPanel flp_Sorted9;
+        private System.Windows.Forms.FlowLayoutPanel flp_Sorted8;
+        private System.Windows.Forms.FlowLayoutPanel flp_Sorted7;
+        private System.Windows.Forms.FlowLayoutPanel flp_Sorted6;
+        private System.Windows.Forms.FlowLayoutPanel flp_Sorted5;
+        private System.Windows.Forms.FlowLayoutPanel flp_Sorted2;
+        private System.Windows.Forms.FlowLayoutPanel flp_Sorted3;
+        private System.Windows.Forms.FlowLayoutPanel flp_Sorted4;
     }
 }
--- MatchGame.cs
+++ MatchGame.cs
@@ -12,6 +12,117 @@ namespace DeweyDecimalStacking
 {
     public partial class MatchGame : Form
     {
+        public MatchGame()
+        {
+            InitializeComponent();
+            flp_Sorted1.AllowDrop = true;
+            flp_Sorted2.AllowDrop = true;
+            flp_Sorted3.AllowDrop = true;
+            flp_Sorted4.AllowDrop = true;
+            flp_Sorted5.AllowDrop = true;
+            flp_Sorted6.AllowDrop = true;
+            flp_Sorted7.AllowDrop = true;
+            flp_Sorted8.AllowDrop = true;
+            flp_Sorted9.AllowDrop = true;
+            flp_Sorted10.AllowDrop = true;
+            flp_Unsorted.AllowDrop = true;
+
+            // DragEventHandler FlowLayoutPanel_DragEnter = null;
+            flp_Unsorted.DragEnter += panel_DragEnter;
+            flp_Sorted1.DragEnter += panel_DragEnter;
+            flp_Sorted2.DragEnter += panel_DragEnter;
+            flp_Sorted3.DragEnter += panel_DragEnter;
+            flp_Sorted4.DragEnter += panel_DragEnter;
+            flp_Sorted5.DragEnter += panel_DragEnter;
+            flp_Sorted6.DragEnter += panel_DragEnter;
+            flp_Sorted7.DragEnter += panel_DragEnter;
+            flp_Sorted8.DragEnter += panel_DragEnter;
+            flp_Sorted9.DragEnter += panel_DragEnter;
+            flp_Sorted10.DragEnter += panel_DragEnter;
+
+            flp_Sorted1.DragDrop += panel_DragDrop;
+            flp_Sorted2.DragDrop += panel_DragDrop;
+            flp_Sorted3.DragDrop += panel_DragDrop;
+            flp_Sorted4.DragDrop += panel_DragDrop;
+            flp_Sorted5.DragDrop += panel_DragDrop;
+            flp_Sorted6.DragDrop += panel_DragDrop;
+            flp_Sorted7.DragDrop += panel_DragDrop;
+            flp_Sorted8.DragDrop += panel_DragDrop;
+            flp_Sorted9.DragDrop += panel_DragDrop;
+            flp_Sorted10.DragDrop += panel_DragDrop;
+            flp_Unsorted.DragDrop += panel_DragDrop;
+
+            label1.MouseDown += act1_MouseDown;
+            label2.MouseDown += act2_MouseDown;
+            label3.MouseDown += act3_MouseDown;
+            label4.MouseDown += act4_MouseDown;
+            label5.MouseDown += act5_MouseDown;
+            label6.MouseDown += act6_MouseDown;
+            label7.MouseDown += act7_MouseDown;
+            label8.MouseDown += act8_MouseDown;
+            label9.MouseDown += act9_MouseDown;
+            label10.MouseDown += act10_MouseDown;
+        }
+
+        //-------------------------------------------------------------------------------------------------
+        //MOUSEDOWN EVENT METHODS
+        /// <summary>
+        /// https://stackoverflow.com/questions/1733912/how-do-i-handle-dragging-of-a-label-in-c
+        /// https://stackoverflow.com/questions/11407068/how-to-drag-and-drop-a-button-from-one-panel-to-another-panel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void act1_MouseDown(object sender, MouseEventArgs e)
+        {
+            label1.DoDragDrop(label1, DragDropEffects.Move);
+        }
+        void act2_MouseDown(object sender, MouseEventArgs e)
+        {
+            label2.DoDragDrop(label2, DragDropEffects.Move);
+        }
+        void act3_MouseDown(object sender, MouseEventArgs e)
+        {
+            label3.DoDragDrop(label3, DragDropEffects.Move);
+        }
+        void act4_MouseDown(object sender, MouseEventArgs e)
+        {
+            label4.DoDragDrop(label4, DragDropEffects.Move);
+        }
+        void act5_MouseDown(object sender, MouseEventArgs e)
+        {
+            label5.DoDragDrop(label5, DragDropEffects.Move);
+        }
+        void act6_MouseDown(object sender, MouseEventArgs e)
+        {
+            label6.DoDragDrop(label6, DragDropEffects.Move);
+        }
+        void act7_MouseDown(object sender, MouseEventArgs e)
+        {
+            label7.DoDragDrop(label7, DragDropEffects.Move);
+        }
+        void act8_MouseDown(object sender, MouseEventArgs e)
+        {
+            label8.DoDragDrop(label8, DragDropEffects.Move);
+
+        }
+        void act9_MouseDown(object sender, MouseEventArgs e)
+        {
+            label9.DoDragDrop(label9, DragDropEffects.Move);
+        }
+        void act10_MouseDown(object sender, MouseEventArgs e)
+        {
+            label10.DoDragDrop(label10, DragDropEffects.Move);
+        }
+
+        void panel_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+        void panel_DragDrop(object sender, DragEventArgs e)
+        {
+            ((Label)e.Data.GetData(typeof(Label))).Parent = (Panel)sender;
+        }
+        //-------------------------------------------------------------------------------------------------     
         public static Dictionary<string, string> BoxLabels = new Dictionary<string, string>()
         {
             {"label1", "000"},
@@ -52,10 +163,6 @@ namespace DeweyDecimalStacking
             {"800", "Literature"},
             {"900", "History & Geography"},
         };
-        public MatchGame()
-        {
-            InitializeComponent();
-        }
 
         private void btn_MatchHome_Click(object sender, EventArgs e)
         {
@@ -66,21 +173,29 @@ namespace DeweyDecimalStacking
 
         private void btn_MatchStart_Click(object sender, EventArgs e)
         {
-            var lastPos = 0;
+            PopulateBoxes();
+        }
 
-            foreach (KeyValuePair<string, string> labels in BoxLabels)
-            {
-                Label l = new Label();
-                l.Name = labels.Key;
-                l.Text = labels.Value;
-                l.Size = new Size(93, 12);
-                l.Location = new Point(0, lastPos);
+    public void PopulateBoxes()
+    {
+        var lastPos = 0;
 
-                lastPos += 15; // Adds 15 to the previous value
-                
-                //flp_Boxes.Controls.Add(l);
-            }
+        foreach (KeyValuePair<string, string> labels in BoxLabels)
+        {
+            Label l = new Label();
+            l.Name = labels.Key;
+            l.Text = labels.Value;
+            l.Size = new Size(93, 12);
+            l.Location = new Point(0, lastPos);
+
+            lastPos += 15; // Adds 15 to the previous value
+
+            flp_Boxes.Controls.Add(l);
         }
     }
+
+    }
+
+
     }
 
